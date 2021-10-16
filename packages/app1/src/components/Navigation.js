2f08fb642f5215b60d7ec53db4c23606d39ba2e2
zhulingying <15692087320@163.com>
2021-10-16 23:57:43 +0800

    perf: :zap: 删除多余代码

--- packages/app1/src/components/Navigation.js
+++ packages/app1/src/components/Navigation.js
@@ -1,12 +1,12 @@
-import React from "lib-app/react";
-import { Link } from "lib-app/react-router-dom";
+import React from 'lib-app/react'
+import { Link } from 'lib-app/react-router-dom'
 
-const style = { border: "1px solid #000", padding: 12 };
+const style = { border: '1px solid #000', padding: 12 }
 
 const Navigation = () => (
   <div style={style}>
-    <Link to="/">Home</Link> - <Link to="/about">About</Link>
+    <Link to='/'>Home</Link> - <Link to='/about'>About</Link>
   </div>
-);
+)
 
-export default Navigation;
+export default Navigation
--- webpack/sharedDependencies.js
+++ webpack/sharedDependencies.js
@@ -1,15 +1,5 @@
 const { dependencies } = require('../package.json')
 
 module.exports = {
-  ...dependencies,
-
-  // react: {
-  //   singleton: true,
-  //   requiredVersion: dependencies.react,
-  // },
-
-  // "react-dom": {
-  //   singleton: true,
-  //   requiredVersion: dependencies["react-dom"],
-  // }
+  ...dependencies
 }
